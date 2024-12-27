e79d10f9da6521e87f8b3d61984aaf5501dd58a9
Miqueias Bento <miqueias.bento.da.silva@gmail.com>
2024-12-27 11:38:47 -0300

    feat: initial commit

---
+++ src/main/java/com/demo/spring/spring_boot_with_docker/SpringBootWithDockerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring.spring_boot_with_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/spring/spring_boot_with_docker/SpringBootWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring.spring_boot_with_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
